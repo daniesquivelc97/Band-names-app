02b10b708320e94b3bd024dd89ef6b866d6d346f
daniesquivelc97 <dani_esquivelc@hotmail.com>
2022-03-29 15:20:58 -0500

    Inicio de aplicación para bandas con sockets

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:band_names_app/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      initialRoute: 'home',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        'home': (_) => HomePage(),
+      },
+    );
+  }
+}
